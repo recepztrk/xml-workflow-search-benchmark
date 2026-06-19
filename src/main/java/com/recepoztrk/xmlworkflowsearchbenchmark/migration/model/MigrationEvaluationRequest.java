d812a38200002384eec0836ad58f4102a2354fd7
Recep Öztürk <recep.ztrk.1907@gmail.com>
2026-06-19 14:11:40 +0300

    Add migration compatibility evaluator request model

---
+++ src/main/java/com/recepoztrk/xmlworkflowsearchbenchmark/migration/model/MigrationEvaluationRequest.java
@@ -0,0 +1,9 @@
+package com.recepoztrk.xmlworkflowsearchbenchmark.migration.model;
+
+import com.recepoztrk.xmlworkflowsearchbenchmark.search.model.ResponseMode;
+import com.recepoztrk.xmlworkflowsearchbenchmark.search.model.SearchMode;
+
+import java.util.List;
+
+/**
+ * Elasticsearch baseline'ına göre aday search engine'lerin sonuç uyumluluğunu ölçmek için kullanılan istek modeli
